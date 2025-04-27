f7c75261df874afab34242f3fc10ff64e0b610d5
Atiqasohail <atiqasohail37@gmail.com>
2025-04-26 18:47:44 -0700

    start cammand added

--- backend/server.mjs
+++ backend/server.mjs
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
 	cors({
-		origin: ['http://localhost:5173'],
+		origin: ['http://localhost:5173'," http://hackathon-nine-puce.vercel.app"],
 		methods: ['GET', 'PUT', 'POST', 'DELETE'],
 		credentials: true,
 		allowedHeaders: ['Content-Type', 'Authorization'],
